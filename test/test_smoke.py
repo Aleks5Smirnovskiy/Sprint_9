0c4d8d3cb0fd42161dc28cc0c3a65eb9c1a02bad
Aleks_Smirnovskiy <aleks711smirnov@mail.ru>
2026-05-01 23:54:47 +0300

    Add smoke test to diagnose CI/CD issues

---
+++ test/test_smoke.py
@@ -0,0 +1,15 @@
+import allure
+
+
+@allure.feature("Smoke test")
+class TestSmoke:
+    @allure.title("Simple smoke test")
+    def test_simple_smoke(self):
+        """Минимальный тест для проверки CI/CD"""
+        assert True
+        
+    @allure.title("Browser opens")
+    def test_browser_opens(self, browser):
+        """Проверка что браузер открывается"""
+        browser.get("https://www.google.com")
+        assert "Google" in browser.title
