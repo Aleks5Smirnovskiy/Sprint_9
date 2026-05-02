6a457c59a83ba93da67b6c0f248abbbe51d4de61
Aleks_Smirnovskiy <aleks711smirnov@mail.ru>
2026-05-02 03:07:40 +0300

    Fix recipe title locator: use contains instead of exact match

--- locators/recipe_details_page_locators.py
+++ locators/recipe_details_page_locators.py
@@ -3,7 +3,8 @@ from selenium.webdriver.common.by import By
 
 class RecipeDetailsPageLocators:
     ADD_TO_CART_BUTTON = (By.XPATH, "//button[contains(., 'Добавить в покупки')]")
+    RECIPE_TITLE = (By.TAG_NAME, "h1")
 
     @staticmethod
     def recipe_title(title: str) -> tuple[str, str]:
-        return By.XPATH, f"//*[self::h1 or self::a][normalize-space()='{title}']"
+        return By.XPATH, f"//h1[contains(., '{title}')]"
